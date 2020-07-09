7f4af25d5d6a946b9da87e1e607da8b56cdb1506
r-erema <roma.a.erema@gmail.com>
2020-07-09 08:04:31 +0300

    dto.go tests

--- internal/service/dto_test.go
+++ internal/service/dto_test.go
@@ -1,1 +1,35 @@
-package service
+package service_test
+
+import (
+	"testing"
+
+	"github.com/r-erema/wapi/internal/infrastructure/whatsapp"
+	"github.com/r-erema/wapi/internal/model"
+	"github.com/r-erema/wapi/internal/service"
+	"github.com/r-erema/wapi/internal/testutil/mock"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDTO(t *testing.T) {
+	dto := service.NewDTO(dtoMocks(t))
+	assert.NotNil(t, dto)
+}
+
+func TestDTOSession(t *testing.T) {
+	conn, sess := dtoMocks(t)
+	dto := service.NewDTO(conn, sess)
+	assert.IsType(t, sess, dto.Session())
+}
+
+func TestDTOWac(t *testing.T) {
+	conn, sess := dtoMocks(t)
+	dto := service.NewDTO(conn, sess)
+	assert.IsType(t, conn, dto.Wac())
+}
+
+func dtoMocks(t *testing.T) (whatsapp.Conn, *model.WapiSession) {
+	c := gomock.NewController(t)
+	return mock.NewMockConn(c), &model.WapiSession{}
+}
